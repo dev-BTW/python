c424dd3a9e833841ebffea4f5f6efd48810e63fc
dev-BTW <parth.more.2304@gmail.com>
2023-09-26 21:37:44 +0530

    added general intro to Lists

---
+++ basics/sequenceOperators.py
@@ -0,0 +1,10 @@
+string1 ="He's "
+string2 = "probably "
+string3 = "mining "
+string4 = "for "
+string5 = "minerals"
+
+print(string1 + string2 + string3 + string4 + string5)
+print("Hello " * 5)
+print("Hello " * (5+4)) #print("Hello " * 5+4) will throw an error
+print("Hello " * 5 + "4")
