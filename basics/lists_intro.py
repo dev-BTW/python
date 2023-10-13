5a50576e458b689059c1045166265c71b9a76dee
dev-BTW <parth.more.2304@gmail.com>
2023-10-13 17:27:25 +0530

    added lists_intro.py file

---
+++ basics/lists_intro.py
@@ -0,0 +1,11 @@
+computer_parts = ["Monitor","CPU","Keyboard","Mouse","Mouse Pad","Speaker"]
+
+for parts in computer_parts:
+    print(parts)
+
+print("_"*30)
+
+print(computer_parts[2]) #prints keyboard
+print(computer_parts[0:3])  #prints from index position 0 to 2 
+print(computer_parts[:3])  #starting index position is 0  by default
+print(computer_parts[-1])
