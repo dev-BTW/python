0e4341ce983792842224728926d682db976fc06b
dev-BTW <parth.more.2304@gmail.com>
2023-12-31 18:33:28 +0530

    added number_lists.py

--- basics/lists_intro.py
+++ basics/lists_intro.py
@@ -1,11 +1,11 @@
-computer_parts = ["Monitor","CPU","Keyboard","Mouse","Mouse Pad","Speaker"]
+computer_parts = ["Keyboard","Mouse","Moniter","CPU","Speaker"]
 
-for parts in computer_parts:
-    print(parts)
-
-print("_"*30)
-
-print(computer_parts[2]) #prints keyboard
-print(computer_parts[0:3])  #prints from index position 0 to 2 
-print(computer_parts[:3])  #starting index position is 0  by default
-print(computer_parts[-1])
+for i in computer_parts:
+    print(i)
+print("\n")
+print(computer_parts[0])
+print(computer_parts[1])
+print("\n")
+print(computer_parts[:])
+print(computer_parts[2:])
+print(computer_parts[:1])
---
+++ basics/number_lists.py
@@ -0,0 +1,15 @@
+even = [2,4,6,8,10]
+odd = [1,3,5,7,9]
+
+print(min(even))
+print(max(even))
+
+print("\n")
+
+print(min(odd))
+print(max(odd))
+
+print("\n")
+
+print(len(even))
+print(len(odd))
