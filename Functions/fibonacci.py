736c3dcc34267f2f23dcc97aeda4415e3da6264e
dev-BTW <parth.more.2304@gmail.com>
2024-03-19 11:43:10 +0530

    updated functions/fibonacci.py

--- Functions/fibonacci.py
+++ Functions/fibonacci.py
@@ -1,14 +1,13 @@
 def fibonacci(n):
-    """returns the 'n'th fibonacci numbers"""
     if 0<=n<=1:
         return n
-    num1,num2= 1,0
-    result =None
+    num1,num2=1,0
+
     for i in range(n-1):
-        result = num1 + num2
+        result = num2+num1 
         num2 = num1
         num1 = result
-    return result
 
-for i in range(36):
-    print(i,fibonacci(i))
+    return result
+for i in range(10):
+    print(i,fibonacci(i))  
