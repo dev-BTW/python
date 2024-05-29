ac256be4dbb9c8e9484a016e6c6f286d29cb4e3b
dev-BTW <parth.more.2304@gmail.com>
2024-05-29 09:13:38 +0530

    Updated sets

--- Sets/setIntersection.py
+++ Sets/setIntersection.py
@@ -2,6 +2,10 @@ evens = set(range(0,200,2))
 odd = set(range(1,200,2))
 squares = {1,4,9,16,25,36,49,64,81,100,121,144,169,196}
 
-print(f"{evens}")
-print(f"{odd}")
+#print(f"{evens}")
+#print(f"{odd}")
+
+print("")
+print(squares.intersection(evens))
+print(squares.intersection(odd))
 
--- Sets/setUnion.py
+++ Sets/setUnion.py
@@ -3,3 +3,4 @@ charlesGarage = {"McLaren F1","McLaren P1","Porsche 911 GT3","BMW m240i"}
 
 print(f"Prints union of two sets: \n{(mikesGarage.union(charlesGarage))}")
 print(f"Prints union of two sets: \n{mikesGarage|charlesGarage}")
+
